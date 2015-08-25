013ea6f56eb40dc5904de362a7ac5c5f20c77cef
eperez <eperez@abargon.com>
2015-08-25 11:35:41 -0500

    change

--- www/js/load_picture.js
+++ www/js/load_picture.js
@@ -35,7 +35,6 @@ function onPhotoDataSuccess(imageURI) {
     //
     cameraImage.attr('src',imageURI);
 	$('#load_photo').toggleClass("active");
-	upload;
 }
 // Called when a photo is successfully retrieved
 //
@@ -62,7 +61,6 @@ function onPhotoURISuccess(imageURI) {
     //
     galleryImage.attr('src',imageURI);
 	$('#load_photo').toggleClass("active");
-	upload;
 }
 // A button will call this function
 //
