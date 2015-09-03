6a2aa452e80fbc4dce3200180093ec4e09d34827
eperez <eperez@abargon.com>
2015-09-03 17:21:09 -0500

    fix

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -688,19 +688,16 @@ angular.module('starter.controllers', [])
 			var remoteFile = url;
         var localFileName = remoteFile.substring(remoteFile.lastIndexOf('/')+1);
         
-        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
-            fileSystem.root.getFile(localFileName, {create: true, exclusive: false}, function(fileEntry) {
-                var localPath = fileEntry.fullPath;
-                //if (device.platform === "Android" && localPath.indexOf("file://") === 0) {
-                    localPath = localPath.substring(7);
-               // }
+
                 var ft = new FileTransfer();
                 ft.download(remoteFile,
-                    localPath, function(entry) {
+                    localFileName, function(entry) {
                         console.log("archivo-descargado");
-                    }, fail);
-            }, fail);
-        }, fail);
+                    }, function(error){
+							console.log("error de descarga");
+					}
+					);
+           
 	}
 	
 	//});
